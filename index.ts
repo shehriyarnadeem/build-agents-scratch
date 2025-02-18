f2193a02a0ceb23c3ac983821b2ce919af422645
shehriyar <shehriyarnadeemy@gmail.com>
2025-02-19 02:21:20 +0500

    tool calls

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config'
-import { runLLM } from './src/llm'
-import { addMessages, getMessages } from './src/memory'
+import { runAgent } from './src/agent'
+import {z} from 'zod'
+
+
 const userMessage = process.argv[2]
 
 if (!userMessage) {
@@ -8,13 +10,17 @@ if (!userMessage) {
   process.exit(1)
 }
 
-await addMessages([{ role: 'user', content: userMessage }])
-const messages = await getMessages()
+const weatherTool ={
+  name:'get_weather',
+  // description:"use this tool to fetch the weather",
+  parameters:z.object({
+    // reasoning:z.string().describe('Why did you pick this tool?')
+  })
+}
 
-const response = await runLLM({
-  messages,
+const messages = await runAgent({
+  userMessage,
+  tools:[weatherTool]
 })
 
-await addMessages([{ role: 'assistant', content: response }])
-
-console.log(response)
+console.log(messages)
--- src/llm.ts
+++ src/llm.ts
@@ -1,12 +1,23 @@
 import type { AIMessage } from '../types'
 import { openai } from './ai'
+import { zodFunction } from 'openai/helpers/zod'
 
-export const runLLM = async ({ messages }: { messages: AIMessage[] }) => {
+export const runLLM = async ({ messages, tools }: { messages: AIMessage[], tools:any[] }) => {
+   const formattedTools = tools.map(zodFunction)
   const response = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
     temperature: 0.1,
     messages,
+    tools:formattedTools,
+    tool_choice:'auto',
+    parallel_tool_calls:false
   })
 
-  return response.choices[0].message.content
+  // We comment out this because if there is a tool call
+  // the response would be like this:
+  // return response.choices[0].tool_calls
+ // return response.choices[0].message.content
+
+ // So intead we just send the message
+    return response.choices[0].message
 }
